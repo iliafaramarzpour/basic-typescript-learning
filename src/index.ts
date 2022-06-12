e18f9a81cee8346d4581cedf5f216b180edd9095
ilia faramarzpour <iliafaramarzpour@gmail.com>
2022-06-12 16:45:46 +0430

    feat : add example variable for first time

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-let message: String = "Hello World !";
+let message: string = "Hello World !";
+
+let phoneNumber: number = 0911111111;
+
+let hasVipPlan: boolean = true;
+
+let userImagesUrl: string[] = ["url", "url", "url"];
+// let userImagesUrl: Array<string> = ["url", "url", "url"];
+
+let userPhoneNumbers: number[] = [09111111111, 09222222222];
+// let userPhoneNumbers: Array<number> = [09111111111, 09222222222];
+
+let userInfo: object = { name: "ilia", family: "faramarzpour" };
